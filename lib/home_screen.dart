699c55d4bae6099c61567f52bc5a05c4ff927047
sobhan ziyaei <sobhan.ziaee73@gmail.com>
2024-02-27 21:49:11 +0330

    add font and continue home screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,10 +11,20 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 219, 219, 219),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(
+          'سنگ کاغذ قیچی',
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+      ),
       body: SafeArea(
         child: Center(
           child: Column(
-            children: [],
+            children: [Image.asset('assets/images/1.png')],
           ),
         ),
       ),
