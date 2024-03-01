1606bad1d04cb39496e4338d2cbf43d774ed37f0
sobhan ziyaei <sobhan.ziaee73@gmail.com>
2024-03-01 16:52:34 +0330

    complete app

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,13 +1,17 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int top = 2;
+  int bottom = 2;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,15 +32,20 @@ class _HomeScreenState extends State<HomeScreen> {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Image.asset('assets/images/1.png', height: 100),
+              Image.asset('assets/images/$top.png', height: 100),
               TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    top = Random().nextInt(3) + 1;
+                    bottom = Random().nextInt(3) + 1;
+                  });
+                },
                 child: const Text(
                   'شروع بازی',
                   style: TextStyle(color: Colors.black, fontSize: 24),
                 ),
               ),
-              Image.asset('assets/images/2.png', height: 100),
+              Image.asset('assets/images/$bottom.png', height: 100),
             ],
           ),
         ),
